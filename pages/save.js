c9cfb1a6f2f10adc15226be27787759bb1a2b3c3
Omotola Olaoti <omotolaolaoti@gmail.com>
2023-03-13 00:13:45 +0100

    save and plan page almost done

--- pages/save.js
+++ pages/save.js
@@ -2,19 +2,19 @@ import MiniHeader from "../props/miniHeader";
 import Navbar from "../Components/Navbar.js";
 import NairaSave from "../public/assets/save/naira-save.svg";
 import Savesections from "../Sections/Savesections.js";
+import Footer from "../Sections/Footer.js";
 const Save = () => {
   return (
     <div className="save">
-      <div className="header-section">
-        <Navbar />
-        <MiniHeader
-          heading="Get on the right side of saving"
-          texts="Saving can be much more than storing money. Automate your savings and earn awesome returns. No penalties. No fees. Absolutely free."
-          buttonText="Get Started Now"
-          headimg={<NairaSave />}
-        />
-        <Savesections />
-      </div>
+      <Navbar />
+      <MiniHeader
+        heading="Get on the right side of saving"
+        texts="Saving can be much more than storing money. Automate your savings and earn awesome returns. No penalties. No fees. Absolutely free."
+        buttonText="Get Started Now"
+        headimg={<NairaSave />}
+      />
+      <Savesections />
+      <Footer />
     </div>
   );
 };
