15599ea1d34b3b94094d31c04d6d668110d9b975
Omotola Olaoti <omotolaolaoti@gmail.com>
2023-03-17 11:05:21 +0100

    resolving issues

--- props/miniHeader.js
+++ props/miniHeader.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 const MiniHeader = ({ heading, texts, buttonText, smallText, headimg }) => {
   return (
     <div className="mini-header grid-two aligncenter">
       <div className="mini-header__texts">
         <p className="small-text">{smallText && smallText}</p>
-=======
-const MiniHeader = ({ heading, texts, buttonText, imgsrc, headimg }) => {
-  return (
-    <div className="mini-header grid-two aligncenter">
-      <div className="mini-header__texts">
->>>>>>> 9b8b9504063135d3430f34c0cd06ae86b499cd96
         <h1>{heading}</h1>
         <p>{texts}</p>
         <button className="btn">{buttonText}</button>
